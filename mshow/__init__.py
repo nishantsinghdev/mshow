29865a86c0efb5bbe2c402eff65848c9c9f1cefa
nishantsinghdev <nishjan1996@gmail.com>
2018-02-21 17:37:16 +0530

    v 0.2

--- mshow/__init__.py
+++ mshow/__init__.py
@@ -1,4 +1,3 @@
 
 def showMsg():
-    return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
-            u'Beiherhund das Oder die Flipperwaldt gersput.')
+    return ("Hello Guys !!\n Its a sample fun() for Suprath Technologies.\n")
