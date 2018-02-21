31e1307944f94b2c70b8e118cbca2cc23195e66b
nishantsinghdev <nishjan1996@gmail.com>
2018-02-21 17:23:06 +0530

    Created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mshow',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/nishantsinghdev/funniest',
+      author='Nishant Singh',
+      author_email='nishant@example.com',
+      license='MIT',
+      packages=['mshow'],
+      zip_safe=False)
