7d49f5f65a4405f0bfa3b7d3d73ea66a01a83751
nishantsinghdev <nishjan1996@gmail.com>
2018-02-21 18:04:45 +0530

    v5.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mshow',
-      version='4.1',
+      version='5.0',
       description='Sample Message Showing Library',
       url='https://github.com/nishantsinghdev/mshow',
       author='Nishant Singh',
